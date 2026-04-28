72faf3498f506cf01662cd25a6bae740bd1c93b5
benyj3-collab <benyj3@gmail.com>
2026-04-28 13:45:06 +0300

    Update server response and logging message

--- backend.js
+++ backend.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("SERVER WORKING");
+  res.send("SERVER OK - WORKING");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on", PORT);
+  console.log("Server running on port", PORT);
 });
